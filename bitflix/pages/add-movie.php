da21f108c06372d1fb2bf7b9e159f671f2142e82
Pave1P <145470076+Pave1P@users.noreply.github.com>
2025-11-17 14:32:45 +0200

    Create add-movie.php

---
+++ bitflix/pages/add-movie.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/../bootstrap.php';
+
+$currentPage = 'add-movie';
+$title = 'Добавить фильм';
+$data = [];
+$contentTemplate = 'add-movie';
+
+include '../templates/layout.php';
